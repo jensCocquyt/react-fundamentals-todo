b276edd82a04a0306f7d79660ff6dfb143f7e66a
jenscocquyt <cocquytjens@gmail.com>
2023-06-07 18:30:49 +0200

    add theme button

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,14 @@ import TodoOverview from "./todo/TodoOverview";
 
 function App() {
   return (
-    <div className="container mx-auto px-4">
-      <TodoOverview />
-    </div>
+    <>
+      <button className=" m-2 p-2 rounded-md  bg-sky-900 text-white">
+        theme
+      </button>
+      <div className="container mx-auto px-4">
+        <TodoOverview />
+      </div>
+    </>
   );
 }
 
